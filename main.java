f89c75a833ab6ff850e9cfea90d29773c2a2b098
mgg895 <mgg895-bt@hotmail.com>
2015-10-01 09:35:56 -0600

    Update main.java

--- main.java
+++ main.java
@@ -58,10 +58,17 @@ public class main {
       paciente3 = vectorCadenas[2].split(",");
       paciente4 = vectorCadenas[3].split(",");
       
-      paciente Paciente1 = new paciente(paciente1[0],paciente1[1],paciente1[2].charAt(0));
-      paciente Paciente2 = new paciente(paciente2[0],paciente2[1],paciente2[2].charAt(0));
-      paciente Paciente3 = new paciente(paciente3[0],paciente3[1],paciente3[2].charAt(0));
-      paciente Paciente4 = new paciente(paciente4[0],paciente4[1],paciente4[2].charAt(0));
+      paciente Paciente1 = new paciente(paciente1[0],paciente1[1],paciente1[2]);
+      paciente Paciente2 = new paciente(paciente2[0],paciente2[1],paciente2[2]);
+      paciente Paciente3 = new paciente(paciente3[0],paciente3[1],paciente3[2]);
+      paciente Paciente4 = new paciente(paciente4[0],paciente4[1],paciente4[2]);
       
+      VectorHeap heap = new VectorHeap();
+      heap.add(Paciente1);
+      heap.add(Paciente2);
+      heap.add(Paciente3);
+      heap.add(Paciente4);
+      paciente [] ArrayHeap = (paciente[]) heap.toArray();
+      System.out.println(ArrayHeap[1].toString());
    }
 }
